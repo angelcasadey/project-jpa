50483f5e5679a28f9e3a11c65adf226a56aeaced
angelcasadey <angel.goca.88@gmail.com>
2023-08-06 20:46:17 -0600

    Version inicial del proyecto project-jpa

---
+++ src/main/java/com/goca/project_jpa/App.java
@@ -0,0 +1,13 @@
+package com.goca.project_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
